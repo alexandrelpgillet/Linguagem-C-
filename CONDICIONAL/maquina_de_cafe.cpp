551f1f9ca0d625786bee53113f7b8cb8e24286eb
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2023-08-20 20:24:53 -0300

    maquina_cafe

--- CONDICIONAL/maquina_de_cafe.cpp
+++ CONDICIONAL/maquina_de_cafe.cpp
@@ -6,22 +6,32 @@ int main(){
     cin>>A1>>A2>>A3;
     
 
-if (A1>A2){
-    if(A2>A3){
-        cout<<(A3*2+A2)*2<<endl;
-    }else{
-    
-    if(A2<A3){
-        cout<<(A3*2+A2)*2<<endl;
-    }else{
-    if(A2==A3){
-    cout<<(A3*2+A2)*2<<endl;
-
-    }
-    }
-}
+if (A3>A1 && A3>A2){
+   
+    cout<<(A2+A1*2)*2<<endl;
 
 }else{
-    if()
+    if(A2>A1 && A2>A3){
+        cout<<(A1+A2)*2<<endl;
+    }else{
+        if(A1>A2 && A1>A3){
+            cout<<(A2+A3*2)*2<<endl;
+        }else{
+            if(A1>A2 && A1==A3){
+                cout<<(A1+A3)*2<<endl;
+            }else{
+            if(A1<A2 && A1==A3){
+                cout<<(A1+A3)*2<<endl;
+            }else{
+                if(A2>A3 && A3==A1){
+                    cout<<(A1+A3)*2<<endl;
+                }if(A2<A3 && A3==A1){
+                    cout<<(A3+A1)*2<<endl;
+                }
+            } 
+            }
+        }
+    }
 }
 }
+    
