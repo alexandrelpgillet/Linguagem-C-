9618754db160083bac38dcd3b152c46e9d472258
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2023-08-20 10:34:46 -0300

    tempo de jogo

---
+++ CONDICIONAL/tempo_de_jogo2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int  hora_inicial , minuto_inicial, hora_final , minuto_final, duracao_horas , duracao_minutos;
+    cin>>hora_inicial>>minuto_inicial>>hora_final>>minuto_final;
+
+    if(hora_final>hora_final){
+        
+        if(minuto_final>minuto_inicial){
+            duracao_horas= hora_final - hora_inicial;
+            duracao_minutos= minuto_final - minuto_final;
+            cout<<"O JOGO DUROU "<<duracao_horas<<" HORA(S) E "<<duracao_minutos<<" MINUTO(S)"<<endl;
+        }else{
+            if(minuto_final<minuto_inicial){
+                duracao_horas=hora_final - hora_inicial;
+                
+            }
+        }
+        
+    }
+}
