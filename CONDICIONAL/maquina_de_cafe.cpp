e843b96d3428fc00cc5c17de82273d99ff0dcf61
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2023-08-21 20:50:28 -0300

    logica certa

--- CONDICIONAL/maquina_de_cafe.cpp
+++ CONDICIONAL/maquina_de_cafe.cpp
@@ -2,36 +2,27 @@
 using namespace std;
 int main(){
     int A1 , A2 , A3;
+    cin >>A1>>A2>>A3;
 
-    cin>>A1>>A2>>A3;
-    
-
-if (A3>A1 && A3>A2){
-   
-    cout<<(A2+A1*2)*2<<endl;
-
-}else{
-    if(A2>A1 && A2>A3){
-        cout<<(A1+A2)*2<<endl;
+    if ((A2 >= A3 && A2 >= A1)){
+        cout<<(A3+A1)*2<<endl;
     }else{
-        if(A1>A2 && A1>A3){
-            cout<<(A2+A3*2)*2<<endl;
+        if (A3 >= A1 && (A1 + A2) <= A3){
+            cout<<(A1 * 4) + (A2 * 2)<<endl;
         }else{
-            if(A1>A2 && A1==A3){
-                cout<<(A1+A3)*2<<endl;
+            if(A1 >= A3 && (A3 + A2) <= A1){
+                cout<<(A3 * 4) + (A2 * 2)<<endl;
+
             }else{
-            if(A1<A2 && A1==A3){
-                cout<<(A1+A3)*2<<endl;
-            }else{
-                if(A2>A3 && A3==A1){
-                    cout<<(A1+A3)*2<<endl;
-                }if(A2<A3 && A3==A1){
-                    cout<<(A3+A1)*2<<endl;
-                }
-            } 
+                cout<<(A3 + A1) * 2<<endl;
             }
+
+
+
         }
     }
 }
-}
-    
+        	
+       
+        	
+        
