8869004e31d37ad742213bef63c3d02cf2f9ea4a
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2023-08-21 19:10:20 -0300

    esboço envolvendo string e int

--- CONDICIONAL/tempo_de_evento.cpp
+++ CONDICIONAL/tempo_de_evento.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
-#include <string>
 using namespace std;
+int main (){
+  string d1 , d2, separador;
+  int dias , total_final , total_inicial  ,dia_inicial , dia_final , hora_inicial , hora_final , minuto_inicial , minuto_final , segundo_inicial , segundo_final;
 
-int main(){
+  cin>>d1>>dia_inicial;
+  cin>>hora_inicial>>separador>>minuto_inicial>>separador>>segundo_inicial;
+  cin>>d2>>dia_final;
+  cin>>hora_final>>separador>>minuto_final>>separador>>segundo_final;
 
-    string Dia;
-   
-    int dia ,inicio_hora, inicio_minuto , inicio_segundo;
-    cin>>Dia;
-    dia = stoi(dia);
-
-    if(Dia == "Dia 5"){
-        cout<<"nice"<<endl;
-    }
-
-
-
+  if(dia_final>dia_inicial){
+    dias = dia_final-dia_inicial;
     
+    total_final = (((((dias*24)+hora_final)*60)+minuto_final)*60)+segundo_final;
+    total_inicial = (((hora_inicial*60)+minuto_inicial)*60)+segundo_inicial;
+    
+    cout<<((total_final-total_inicial)/3600)/24<<" dia(s)"<<endl;
+    cout<<((total_final-total_inicial)/3600)%24<<" hora(s)"<<endl;
+    cout<<(((total_final-total_inicial)/3600)%24)/60<<" minuto(s)"<<endl;
+    cout<<(((total_final-total_inicial)/3600)%24)%60<<" segundos(s)"<<endl;
+
+
+  }
+
+
+
 }
