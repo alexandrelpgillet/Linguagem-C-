63fef42da6cf2e6330407c5ded3add51009a9060
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2023-08-23 17:52:50 -0300

    tempo_de_evento( entrada com  envolvendo strings e int)

--- CONDICIONAL/tempo_de_evento.cpp
+++ CONDICIONAL/tempo_de_evento.cpp
@@ -1,26 +1,48 @@
 #include <iostream>
 using namespace std;
-int main (){
-  string d1 , d2, separador;
-  int dias , total_final , total_inicial  ,dia_inicial , dia_final , hora_inicial , hora_final , minuto_inicial , minuto_final , segundo_inicial , segundo_final;
+int main(){
 
-  cin>>d1>>dia_inicial;
-  cin>>hora_inicial>>separador>>minuto_inicial>>separador>>segundo_inicial;
-  cin>>d2>>dia_final;
-  cin>>hora_final>>separador>>minuto_final>>separador>>segundo_final;
+  string s;
+  int dia_inicial , dia_final , hora_inicial , hora_final , minuto_inicial , minuto_final , segundo_inicial , segundo_final, total , horas , minutos , segundos , intervalo;
+  
+  cin>> s >> dia_inicial;
+  cin>> hora_inicial >> s >> minuto_inicial >> s >> segundo_inicial;
+  cin >> s >> dia_final;
+  cin>> hora_final >> s >> minuto_final >> s >> segundo_final;
 
-  if(dia_final>dia_inicial){
-    dias = dia_final-dia_inicial;
+
+
+
+
+
+  if(dia_inicial == dia_final){
     
-    total_final = (((((dias*24)+hora_final)*60)+minuto_final)*60)+segundo_final;
-    total_inicial = (((hora_inicial*60)+minuto_inicial)*60)+segundo_inicial;
+    total= (hora_final*3600 + minuto_final*60 + segundo_final) - (hora_inicial*3600+minuto_inicial*60+segundo_inicial);
     
-    cout<<((total_final-total_inicial)/3600)/24<<" dia(s)"<<endl;
-    cout<<((total_final-total_inicial)/3600)%24<<" hora(s)"<<endl;
-    cout<<(((total_final-total_inicial)/3600)%24)/60<<" minuto(s)"<<endl;
-    cout<<(((total_final-total_inicial)/3600)%24)%60<<" segundos(s)"<<endl;
+    
+    
+    
+    
+    cout<<"0 dia(s)"<<endl;
+    cout<<((hora_final*3600 + minuto_final*60 + segundo_final) - (hora_inicial*3600+minuto_inicial*60+segundo_inicial))/3600<<" hora(s)"<<endl;
+    cout<<(((hora_final*3600 + minuto_final*60 + segundo_final) - (hora_inicial*3600+minuto_inicial*60+segundo_inicial))%3600)/60<<" minuto(s)"<<endl;
+    cout<<(((hora_final*3600 + minuto_final*60 + segundo_final) - (hora_inicial*3600+minuto_inicial*60+segundo_inicial))%3600)%60<<" segundo(s)"<<endl;
+   
+    
 
 
+  }else{
+
+    if(dia_final>dia_inicial){
+      
+      intervalo = dia_final - dia_inicial;
+      total = ((hora_final)*3600 + minuto_final*60 + segundo_final+intervalo*86400) - (hora_inicial*3600+minuto_inicial*60+segundo_inicial);
+      cout<<total/86400<<" dia(s)"<<endl;
+      cout<<(total%86400)/3600<<" hora(s)"<<endl;
+      cout<<((total%86400)%3600)/60<<" minuto(s)"<<endl;
+      cout<<((total%86400)%3600)%60<<" segundo(s)"<<endl;
+
+    }
   }
 
 
