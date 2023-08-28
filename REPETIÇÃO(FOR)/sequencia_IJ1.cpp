ecde649d51d93a6bbc5b3dfdd8c30d36fda5bf60
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2023-08-28 20:43:56 -0300

    Fatorial

---
+++ REPETIÇÃO(FOR)/sequencia_IJ1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int I , J;
+
+    for(I=1,J=60;I<=36,J>=0;I+=3,J-=5 ){
+     cout<<"I="<<I<<" "<<"J="<<J<<endl;
+   
+    }
+}
---
+++ REPETIÇÃO(FOR)/sequencia_IJ3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+   for(int I=1 , J=7 ; J>=5 ; J--){
+    cout<<"I="<<I<<" "<<"J="<<J<<endl;
+   }
+
+   for(int I=3, J=9 ; J>=7;J--){
+    cout<<"I="<<I<<" "<<"J="<<J<<endl;
+
+   }
+
+   for(int I= 5 , J=11 ; J>=9;J--){
+    cout<<"I="<<I<<" "<<"J="<<J<<endl;
+
+   }
+
+   for(int I=7 , J=13 ; J>=11; J--){
+    cout<<"I="<<I<<" "<<"J="<<J<<endl;
+
+   }
+
+   for(int I=9 , J=15 ; J>=13 ; J--){
+    cout<<"I="<<I<<" "<<"J="<<J<<endl;
+
+   }
+}
