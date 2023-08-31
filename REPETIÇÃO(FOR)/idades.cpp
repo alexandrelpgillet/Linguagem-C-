0e528f69919dd03c7f9036ee016f4f724eae368c
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2023-08-30 22:15:45 -0300

    Repetição for com entradas infinitas

---
+++ REPETIÇÃO(FOR)/idades.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    
+    int X , Y;
+    float num , soma , divisor , soma2 , resultado;
+    
+   
+    for(cin>>X , Y=X, num=0 ,soma=0 ; X>0 ; num++, cin>>X , soma+=X){
+
+       
+       
+        soma2= soma+Y;
+        divisor=num+1;
+        
+        
+       
+
+    }
+
+    if(X<=0){
+
+            resultado=soma2/divisor;
+            cout<<fixed<<setprecision(2);
+            cout<<resultado<<endl;
+            
+            
+            
+        }
+        
+}
