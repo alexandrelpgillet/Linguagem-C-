06a01e889311a20cc7675a33db70ec51d51616ab
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2023-09-07 14:07:33 -0300

    Sapo
    if(altura3 !=0){
            if((altura2 - altura3) > P || (altura3- altura2)> P ){
    
                loser+=1;
    
            }
           }

--- REPETIÇÂO2/pulo_sapo.cpp
+++ REPETIÇÂO2/pulo_sapo.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int P, N , repet , y , x  , altura , altura2, altura_atual , proxima_altura;
 
+    int P , N , altura , altura2 , altura3 , loser;
+    cin>>P>>N;
+    altura = 0;
+    loser = 0;
+    
+
+    for( P , N  ; N>0 ; N--){
+        
+        altura3 =altura;
+        cin>>altura;
+        altura2 = altura;
+
+        
+       if(altura3 !=0){
+        if((altura2 - altura3) > P || (altura3- altura2)> P ){
+
+           
+            
+            loser+=1;
+            
+        }
+       }
+        
 
     
-    repet=0;
-    cin>>P>>N;
-
-    for(P,N ,repet ; repet<=N ; repet++){
-
-
-        
-       
-        altura_atual = altura;
-        cin>>altura;
-        cin>>altura2;
-        
-        altura_atual = altura;
-        proxima_altura = altura2;
-
-        cout<<altura_atual<<endl;
-        cout<<proxima_altura<<endl;
-
-
-        
-
-       
 
     }
 
+    if(loser !=0){
 
 
+    
+        cout<<"GAME OVER"<<endl;
+
+            
+        }else{
+            cout<<"YOU WIN"<<endl;
+        }
 }
