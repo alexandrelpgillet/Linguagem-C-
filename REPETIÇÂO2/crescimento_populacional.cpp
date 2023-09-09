48ded1f0c4fc278678e9e47d086086ad8a7fe0ab
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2023-09-09 11:41:31 -0300

    Crescimento exponencial (população)

--- REPETIÇÂO2/crescimento_populacional.cpp
+++ REPETIÇÂO2/crescimento_populacional.cpp
@@ -1,23 +1,67 @@
 #include <iostream>
-#include <math.h>
+
 using namespace std;
 int main(){
 
-    int PA , PB , T , A;
-    double G1 , G2;
+    int  T  ,  PA , PB;
+    double G1 , G2 , G3 , A ;
 
    
 
-    while(cin>>T){
+    cin>>T;
+
 
         for(T ; T>0 ; T--){
 
             cin>>PA>>PB>>G1>>G2;
 
-            PA * (pow((1+(G1/100 - G2/100)) , A ) ) = PB;
+            G3 = G1 - G2;
+            
+
+           for(A =0, G3; PA<=PB ;  ){
+            
+            A++;
+            
+            PA*=(1+G1/100.0);
+            PB*=(1 + G2/100.0);
+            
+           
+            
+
+            if(A>100){
+                PA+=PB;
+                
+            }
+
+            
+            
+           
+            
+
+            
+           }
+
+           if(PA >= PB){
+            
+            if(A>100){
+
+                cout<<"Mais de 1 seculo."<<endl;
+                
+            }else{
+                cout<<A<<" anos."<<endl;
+            }
+
+            }
+
+           
+            
 
         }
 
-    }
-
+        
+       
 }
+
+
+
+
