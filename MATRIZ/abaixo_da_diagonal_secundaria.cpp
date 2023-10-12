8f6fb9358e7fa16810a314ce4eb3aaf7538435c0
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2023-10-11 21:17:17 -0300

    Create abaixo_da_diagonal_secundaria.cpp

---
+++ MATRIZ/abaixo_da_diagonal_secundaria.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+
+  char O;
+  float M[12][12] , R;
+  int i , i2 , i3;
+
+  cin>>O;
+  R=0;
+
+  for(i=0 ; i<12 ; i++){
+    for(i2=0 ; i2<12 ; i2++){
+      cin>>M[i][i2];
+    }
+  }
+
+  for(i=0 , i3=11 ; i<12 ; i++ , i3--){
+    for(i2=11; i2>i3; i2--){
+      R+=M[i][i2];
+     
+    }
+  }
+
+  if(O='M'){
+    R=R/66;
+  }
+  
+  cout<<fixed<<setprecision(1);
+  cout<<R<<endl;
+}
