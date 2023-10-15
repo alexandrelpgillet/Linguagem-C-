d3265586cacaadf80008d780db7fbad5db94cb67
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2023-10-15 17:45:24 -0300

    Create fração.cpp

---
+++ REPETICÃO3/fração.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main(){
+    
+    int  N;
+    long double var , valor;
+    
+    cin>>N;
+    
+    for(N , var=0; N>0 ; N--){
+        var = 1.0 / (var + 6.0);
+
+       
+        
+        
+        
+    }
+    
+    
+    cout<<fixed<<setprecision(10);
+    cout<<3+var<<endl;
+}
